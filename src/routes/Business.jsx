68b8e4f0f04fd88246c5437d0cc7484bdb506c00
BDeBruyne <debruynebrad@gmail.com>
2023-03-09 22:44:08 -0700

    Update

--- src/routes/Business.jsx
+++ src/routes/Business.jsx
@@ -1,16 +1,10 @@
 import React from 'react'
-import Newsfeed from '../components/Newsfeed'
-import Thumbnails from '../components/Thumbnails'
-import TopFive from '../components/TopFive'
-import Trending from '../components/Trending'
+
 
 function Business() {
   return (
     <div>
-        <Newsfeed />  
-        <TopFive />
-        <Trending />
-        <Thumbnails />
+        
     </div>
   )
 }
