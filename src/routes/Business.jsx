0891ac4036237aa72b13259578fabf8d5d266faa
BDeBruyne <debruynebrad@gmail.com>
2023-03-12 14:08:13 -0600

    Update

--- src/routes/Business.jsx
+++ src/routes/Business.jsx
@@ -9,35 +9,57 @@ function Business() {
           <p></p>
         </div>
 
-        <><div className='mt-8'></div><div className='px-2 max-w-[1140px] w-full mx-auto text-primary shadow-xl pt-2 mb-8 border border-[#85bb65] pb-2'>
-            <iframe src="https://www.feedgrabbr.com/inframe/?widgetid=fb38217d97342248bfbf55450" title='NewsFeed' className="flex max-w-[1140px] w-full h-[600px] mx-auto"></iframe>
-            {/* <iframe src="https://www.feedgrabbr.com/inframe/?widgetid=fb38217d97342248bfbf55450" allowtransparency="true" style="border:0;background-color: transparent;width:100%;height:600px" ></iframe> */} 
+        <>
+        <div className='mt-8'></div><div className=' max-w-[1140px] w-full mx-auto text-primary shadow-xl mb-8 border border-[#85bb65] p-3'>
+            <iframe src="https://www.feedgrabbr.com/inframe/?widgetid=fb38217d97342248bfbf55450" title='NewsFeed' className="flex max-w-[1140px] w-full h-[800px] mx-auto"></iframe>
+            {/* <iframe src="https://www.feedgrabbr.com/inframe/?widgetid=b6d85f9a05d2d3543231a9653" allowtransparency="true" style="border:0;background-color: transparent;width:100%;height:800px" ></iframe> */} 
         </div>
+                        
         
-        <div className='mt-8'></div><div className=' max-w-[1140px]  w-full mx-auto text-primary shadow-xl mb-8 p-2 pb-3 border border-[#85bb65]'>
-            <iframe src="https://www.feedgrabbr.com/inframe/?widgetid=68ddb34ffd86008eb7ac204df" title='Thumbnails' className="flex max-w-[1140px] w-[100%] h-[800px] mx-auto "></iframe>
-            {/*<iframe src="https://www.feedgrabbr.com/inframe/?widgetid=68ddb34ffd86008eb7ac204df" allowtransparency="true" style="border:0;background-color: transparent;width:100%;height:800px" ></iframe>*/}
-        </div>
-        
-        <div className='mt-8'></div><div className=' max-w-[1140px]  w-full mx-auto text-primary shadow-xl mb-8  border border-[#85bb65]'>
-            <iframe src="https://www.feedgrabbr.com/inframe/?widgetid=cfeea8b208439ea84533ccfaf" title='Thumbnails' className="flex max-w-[1140px] w-[100%] h-[900px] mx-auto "></iframe>
-            {/*<iframe src="https://www.feedgrabbr.com/inframe/?widgetid=cfeea8b208439ea84533ccfaf" allowtransparency="true" style="border:0;background-color: transparent;width:100%;height:900px" ></iframe>*/}
-        </div>
 
         
         
-        <div className='flex flex-col sm:flex-row border shadow-xl pl-2 max-w-[1140px] w-full mx-auto border-[#85bb65] mt-9'>
-      <div className='max-w-[760px] w-full mx-auto flex-col'>
-      <iframe  width="100%" height="311px" className='flex pt-3' src="https://www.youtube.com/embed/z6mrjWWdJhk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; allow-full-screen; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen='allowFullScreen'></iframe>
-      <iframe  width="100%" height="311px" className='flex pt-3 pb-3' src="https://www.youtube.com/embed/khAt5UqzvI0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; allow-full-screen; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen='allowFullScreen'></iframe>     
-      </div>
-      <div className=''></div><div className='flex w-full pt-1'>
-            <iframe src="https://www.feedgrabbr.com/inframe/?widgetid=667f1220b3b1afbdfe740b082" title='Thumbnails' className="flex w-[100%] h-[616px] "></iframe>
-            {/*<iframe src="https://www.feedgrabbr.com/inframe/?widgetid=667f1220b3b1afbdfe740b082" allowtransparency="true" style="border:0;background-color: transparent;width:100%;height:900px" ></iframe>*/}
+        <div className='flex flex-col sm:flex-row mt-8 max-w-[1140px]  w-full  mx-auto text-primary shadow-xl mb-8 border border-[#85bb65]'>
+            <div className='sm:w-[50%] sm:border border-r-[#85bb65]'>
+            <iframe src="https://www.feedgrabbr.com/inframe/?widgetid=68ddb34ffd86008eb7ac204df" title='Thumbnails' className="flex max-w-[1140px] w-[100%] h-[955px] mr-auto p-2 pb-3"></iframe>
+            {/*<iframe width="560" height="315" src="https://www.youtube.com/embed/AXfLs4TcQa8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>*/}
+            </div>
+
+            <div className='w-full sm:w-[50%] h-[933px] mx-auto flex-col px-3'>
+              <iframe  width="100%" height="311px" className='flex pt-3' src="https://www.youtube.com/embed/AXfLs4TcQa8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; allow-full-screen; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen='allowFullScreen'></iframe>
+              <iframe  width="100%" height="311px" className='flex pt-3 ' src="https://www.youtube.com/embed/p3be7bvZkUU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; allow-full-screen; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen='allowFullScreen'></iframe>
+              <iframe  width="100%" height="311px" className='flex pt-3 ' src="https://www.youtube.com/embed/OpOdEnKlgA4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; allow-full-screen; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen='allowFullScreen'></iframe>     
+            </div>
+        </div>
+
+        <div className='mt-8'></div><div className=' max-w-[1140px]  w-full mx-auto text-primary shadow-xl  border border-[#85bb65] p-1'>
+            <iframe src="https://www.feedgrabbr.com/inframe/?widgetid=cfeea8b208439ea84533ccfaf" title='Thumbnails' className="flex max-w-[1140px] w-[100%] h-[900px] mx-auto "></iframe>
+            {/*<iframe src="https://www.feedgrabbr.com/inframe/?widgetid=32c153717c4e5cc9b923122df" allowtransparency="true" style="border:0;background-color: transparent;width:100%;height:700px" ></iframe>/> */}
+        </div>
+        
+        <div className='mt-8'></div><div className='max-w-[1140px] w-full mx-auto text-primary shadow-xl px-3 pt-3 mb-8 border border-[#85bb65] '>
+            <iframe src="https://www.feedgrabbr.com/inframe/?widgetid=9f5a6325fa51dfd7fea3afdc2" title='NewsFeed' className="flex max-w-[1140px] w-full h-[500px] mx-auto"></iframe>
+            {/* <iframe src="https://www.feedgrabbr.com/inframe/?widgetid=9f5a6325fa51dfd7fea3afdc2" allowtransparency="true" style="border:0;background-color: transparent;width:100%;height:500px" ></iframe> */} 
+        </div>
+
+        <div className='flex flex-col sm:flex-row mt-8 max-w-[1140px]  w-full  mx-auto text-primary shadow-xl mb-8 border border-[#85bb65]'>
+          
+
+            <div className='w-full sm:w-[50%] h-[622px] mx-auto flex-col px-3'>             
+              <iframe  width="100%" height="311px" className='flex pt-3 ' src="https://www.youtube.com/embed/z6mrjWWdJhk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; allow-full-screen; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen='allowFullScreen'></iframe>
+              <iframe  width="100%" height="311px" className='flex pt-3 pb-3' src="https://www.youtube.com/embed/khAt5UqzvI0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; allow-full-screen; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen='allowFullScreen'></iframe>     
+            </div>  
+            
+            <div className='sm:w-[50%] sm:border border-r-[#85bb65] pt-1'>
+            <iframe src="https://www.feedgrabbr.com/inframe/?widgetid=667f1220b3b1afbdfe740b082" title='Thumbnails' className="flex max-w-[1140px] w-[100%] h-[606px] mr-auto "></iframe>
+            {/*<iframe src="https://www.feedgrabbr.com/inframe/?widgetid=3afbae6d6d924c44bb160f587" allowtransparency="true" style="border:0;background-color: transparent;width:100%;height:630px" ></iframe>*/}
+            </div>
         </div>
       
-      {/*<iframe width="560" height="315" src="https://www.youtube.com/embed/z6mrjWWdJhk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
-    </div>
+      {/*<iframe width="560" height="315" src="https://www.youtube.com/embed/xPl8H2jZ_UI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
+    
+        
+        
         </>
 
 
